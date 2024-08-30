687755ab9fb24b1b52161c25a30d687bede3e741
Fermín Ares <fermin@ares.uy>
2024-08-30 16:33:39 -0300

    tutorial content adjustments

--- src/components/Tutorial.tsx
+++ src/components/Tutorial.tsx
@@ -18,7 +18,7 @@ import Popover from "./Popover"
 interface TutorialComponentProps { board: Board }
 
 export default function TutorialComponent({ board }: TutorialComponentProps) {
-  const t = useTranslations("Pages.Tutorial")
+  const t = useTranslations()
   const router = useRouter()
   const [tutorialStep, setTutorialStep] = useState(0)
   const [playerBoard, setPlayerBoard] = useState<PlayerBoard>(getPlayerBoardData("barco", 2, false, board))
@@ -60,11 +60,11 @@ export default function TutorialComponent({ board }: TutorialComponentProps) {
     setTutorialStep((current) => current + 1)
   }
 
-  const contiuneBtn = useCallback((label = "Continue") => (
+  const contiuneBtn = useCallback((label = t("Labels.continue")) => (
     <button type="button" onClick={() => setTutorialStep((current) => current + 1)}>
       {label}
     </button>
-  ), [])
+  ), [t])
 
   const translationOptions: RichTranslationValues = useMemo(() => ({ p: (chunks: ReactNode) => <p>{chunks}</p> }), [])
   
@@ -78,90 +78,90 @@ export default function TutorialComponent({ board }: TutorialComponentProps) {
             <Modal id="tutorial-modal" labelledBy="tutorial-modal-title" open={true} closeable={false}>
               {tutorialStep === 0 && (
                 <div>
-                  {t("t1")}
-                  {t.rich("m1", { objectRevealedIcon, enemyIcon: enemyIcons.octopus, ...translationOptions })}
+                  {t("Pages.Tutorial.t1")}
+                  {t.rich("Pages.Tutorial.m1", { objectRevealedIcon, enemyIcon: enemyIcons.octopus, ...translationOptions })}
                 </div>
               )}
               {tutorialStep === 1 && (
                 <div>
-                  {t("t2")}
-                  {t.rich("m2", translationOptions)}
+                  {t("Pages.Tutorial.t2")}
+                  {t.rich("Pages.Tutorial.m2", translationOptions)}
                 </div>
               )}
               {tutorialStep === 12 && (
                 <div>
-                  {t("t2")}
-                  {t.rich("m2", translationOptions)}
+                  {t("Pages.Tutorial.t12")}
+                  {t.rich("Pages.Tutorial.m12", translationOptions)}
                 </div>
               )}
-              {contiuneBtn(tutorialStep === 12 ? "Finish tutorial" : undefined)}
+              {contiuneBtn(tutorialStep === 12 ? t("Labels.finish-tutorial") : undefined)}
             </Modal>
           )}
           {(tutorialStep === 2) && (
             <div className="tutorial-card tutorial-card--2">
-              <Popover open title={t("t3")}>
-                {t.rich("m3", translationOptions)}
+              <Popover open title={t("Pages.Tutorial.t3")}>
+                {t.rich("Pages.Tutorial.m3", translationOptions)}
               </Popover>
             </div>
           )}
           {(tutorialStep === 3) && (
             <div className="tutorial-card tutorial-card--3">
-              <Popover open title={t("t4")}>
-                {t.rich("m4", translationOptions)}
+              <Popover open title={t("Pages.Tutorial.t4")}>
+                {t.rich("Pages.Tutorial.m4", translationOptions)}
               </Popover>
             </div>
           )}
           {(tutorialStep === 4) && (
             <div className="tutorial-card tutorial-card--4">
-              <Popover open title={t("t5")}>
-                {t.rich("m5", translationOptions)}
+              <Popover open title={t("Pages.Tutorial.t5")}>
+                {t.rich("Pages.Tutorial.m5", translationOptions)}
                 {contiuneBtn()}
               </Popover>
             </div>
           )}
           {(tutorialStep === 5) && (
             <div className="tutorial-card tutorial-card--5">
-              <Popover open title={t("t6")}>
-                {t.rich("m6", { objectIcon: board.cards[0][0].object?.icon, ...translationOptions })}
+              <Popover open title={t("Pages.Tutorial.t6")}>
+                {t.rich("Pages.Tutorial.m6", { objectIcon: board.cards[0][0].object?.icon, ...translationOptions })}
               </Popover>
             </div>
           )}
           {(tutorialStep === 6) && (
             <div className="tutorial-card tutorial-card--6">
-              <Popover open title={t("t7")}>
-                {t.rich("m7", { objectIcon: board.cards[0][0].object?.icon, shrimpIcon, playersIcon, ...translationOptions })}
+              <Popover open title={t("Pages.Tutorial.t7")}>
+                {t.rich("Pages.Tutorial.m7", { objectIcon: board.cards[0][0].object?.icon, shrimpIcon, playersIcon, ...translationOptions })}
                 {contiuneBtn()}
               </Popover>
             </div>
           )}
           {(tutorialStep === 7) && (
             <div className="tutorial-card tutorial-card--7">
-              <Popover open title={t("t8")}>
-                {t.rich("m8", translationOptions)}
+              <Popover open title={t("Pages.Tutorial.t8")}>
+                {t.rich("Pages.Tutorial.m8", translationOptions)}
                 {contiuneBtn()}
               </Popover>
             </div>
           )}
           {(tutorialStep === 8) && (
             <div className="tutorial-card tutorial-card--8">
-              <Popover open title={t("t9")}>
-                {t.rich("m9", translationOptions)}
+              <Popover open title={t("Pages.Tutorial.t9")}>
+                {t.rich("Pages.Tutorial.m9", translationOptions)}
                 {contiuneBtn()}
               </Popover>
             </div>
           )}
           {(tutorialStep === 10) && (
             <div className="tutorial-card tutorial-card--10">
-              <Popover open title={t("t10")} position="right">
-                {t.rich("m10", { objectIcon: board.cards[0][5].object?.icon, objectRevealedIcon, ...translationOptions })}
+              <Popover open title={t("Pages.Tutorial.t10")} position="right">
+                {t.rich("Pages.Tutorial.m10", { objectIcon: board.cards[0][5].object?.icon, objectRevealedIcon, ...translationOptions })}
                 {contiuneBtn()}
               </Popover>
             </div>
           )}
           {(tutorialStep === 11) && (
             <div className="tutorial-card tutorial-card--11">
-              <Popover open title={t("t11")} position="right">
-                {t.rich("m11", { shrimpIcon, ...translationOptions })}
+              <Popover open title={t("Pages.Tutorial.t11")} position="right">
+                {t.rich("Pages.Tutorial.m11", { shrimpIcon, ...translationOptions })}
                 {contiuneBtn()}
               </Popover>
             </div>
